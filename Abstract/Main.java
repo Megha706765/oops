fd8a6957cb905f49643f287c3b44bad9b2174988
Megha706765 <meghathakur2003123@gmail.com>
2026-03-10 23:39:05 +0530

    AbstractCode

---
+++ Abstract/Main.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(reduceTosingle(n));
+    }
+
+    public static int reduceTosingle(int n) {
+        if (n <= 9) {
+            return n;
+        }
+        int sum = 0;
+        while (n != 0) {
+            int rem = n % 10;
+            sum = sum + rem;
+            n = n / 10;
+        }
+        return reduceTosingle(sum);
+    }
+}
